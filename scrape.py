dd1a7197d07c6250ef0badbbd0e0bfc0654afb03
jacobericson <jakematt2004@yahoo.com>
2019-09-23 18:04:18 +0000

    Various scrape script updates
    * Add error logging and writing to file
    
    * Don't clear bikes table unless the scrape was successful
    
    * Add exponential backoff to scraping trips

--- scrape.py
+++ scrape.py
@@ -20,7 +20,7 @@ class Scraper:
     def main(self):
         try:
             self.scrapebikes(self.conn, self.cur)
-            self.scrapetrips(self.lastran, self.conn, self.cur)
+            self.scrapetrips(self.lastran, 1800, self.conn, self.cur)
             while True: 
                     time.sleep(1)
         except KeyboardInterrupt:
@@ -72,8 +72,17 @@ class Scraper:
         print('Scraping bikes')
         payload = {'format': 'json', 'fields': 'lat,lon,isReserved,isDisabled,createdAt,lastSeenAtPos,linger_time_minutes,linger_time_hours,linger_time_days', 'authorization': self.apikey}
         req = requests.get('http://nodes.geoservices.tamu.edu/api/veoride/bikes/', params=payload)
-        print('Finished scraping bikes')
-        self.insertbikes(req, conn, cur)
+        if req.status_code != 200 or not req.json():
+            print('Request Failed at ' + str(datetime.datetime.now()))
+            errorfile = open('error.log', 'a+')
+            errorfile.write('Request Failed at ' + str(datetime.datetime.now()))
+            errorfile.close()
+            biketimer = threading.Timer(600.0, self.scrapebikes, [conn, cur])
+            biketimer.daemon = True
+            biketimer.start()
+        else: 
+            print('Finished scraping bikes')
+            self.insertbikes(req, conn, cur)
 
     def insertbikes(self, request, conn, cur):
         print('Starting bikes insert')
@@ -90,19 +99,19 @@ class Scraper:
         biketimer.daemon = True
         biketimer.start()
 
-    def scrapetrips(self, starttime, conn, cur):
+    def scrapetrips(self, starttime, nexttime, conn, cur):
         print('Strarting trips scrape')
         payload = {'format': 'json', 'end_time_gte': starttime, 'fields': 'start_time,end_time,start_latitude,start_longitude,end_latitude,end_longitude', 'authorization': self.apikey}
         req = requests.get('https://nodes.geoservices.tamu.edu/api/veoride/trips/', params=payload)
         print('Finished trips scrape')
-        if req.json():
-            self.inserttrips(req, conn, cur)
-        else:
-            print('No recent trips, waiting 30 minutes before trying again')
-            self.lastran = int(time.time() * 1000)
-            triptimer = threading.Timer(1800.0, self.scrapetrips, [self.lastran, conn, cur])
+        if not req.json():
+            nexttime = nexttime * 1.5
+            print('No recent trips, waiting {} minutes before trying again'.format(nexttime/60))
+            triptimer = threading.Timer(nexttime, self.scrapetrips, [self.lastran, nexttime, conn, cur])
             triptimer.daemon = True
             triptimer.start()
+        else:
+            self.inserttrips(req, conn, cur)
     def inserttrips(self, request, conn, cur):
         print('Starting trips insert')
         data = request.json()
@@ -113,7 +122,7 @@ class Scraper:
         conn.commit()
         print('Finished trips insert')
         self.lastran = int(time.time() * 1000)
-        triptimer = threading.Timer(1800.0, self.scrapetrips, [self.lastran, conn, cur])
+        triptimer = threading.Timer(1800.0, self.scrapetrips, [self.lastran, 0, conn, cur])
         triptimer.daemon = True
         triptimer.start()
 
